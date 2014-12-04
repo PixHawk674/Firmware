006023708f5356776df4dcc24a07eb01a04fe520
James Jackson <superjax08@gmail.com>
2014-12-03 17:05:41 -0700

    added new PID loops based on C++, not C.  Also duplicated fw_att_control to the new MAGICC version.

---
+++ src/modules/systemlib/UAVpid/UAVpid.cpp
@@ -0,0 +1,80 @@
+#include "UAVpid.h"
+
+namespace UAVpid_UAV
+{
+UAVpid::UAVpid(double* input, double* output, double* setpoint, 
+	 double kp, double ki, double kd, double ts, 
+	 double tau, double upper_limit, double lower_limit)
+{
+	_output   = output;
+	_input    = input;
+	_setpoint = setpoint;
+	_tau      = tau;
+	_ts 	  = ts;
+
+	setOutputLimits(upper_limit, lower_limit);
+	setGains(kp,ki,kd);
+	reset();
+
+}
+
+bool UAVpid::setOutputLimits(double upper_limit, double lower_limit)
+{
+	_upperLimit = upper_limit;
+	_lowerLimit = lower_limit;
+	return 1;
+}
+
+bool UAVpid::setGains(kp,ki,kd)
+{
+	_kp = kp;
+	_ki = ki;
+	_kd = kd;
+	return 1;
+}
+
+bool UAVpid::reset()
+{
+	_integrator = 0;
+	_differentiator = 0;
+	_error_dl = 0;
+	return 1;
+}
+
+bool UAVpid::compute()
+{
+	double error = *_setpoint - *_input;
+	_integrator += (_ts/2)*(error+_prev_dl);
+	_differentiator = (2*_tau-_ts)/(2*_tau+_ts)*_differentiator+2/(2*_tau+_ts)*(error-_error_dl);
+	_error_dl = _error;
+
+	u = sat(kp*error+ki*_integrator+kd*_differentiator, _upperLimit, _lowerLimit);
+	// integrator anti-windup scheme
+	if(_ki!=0)
+	{
+		u_unsat = kp*error+ki*_integrator+kd*_differentiator;
+		if(u!=u_unsat)
+		{
+			_integrator = _integrator + _ts/_ki*(u-u_unsat);
+		}
+	}
+	*_output = u;
+}
+
+double UAVpid::sat(double input, double upper_limit, double lower_limit)
+{
+	if(u>upper_limit)
+	{
+		u = upper_limit;
+	}
+	if(u<lower_limit)
+	{
+		u = lower_limit;
+	}
+	return u;
+}
+
+double UAVpid::getKp(){return _kp;}
+double UAVpid::getKi(){return _ki;}
+double UAVpid::getKd(){return _kd;}
+} // end namespace
